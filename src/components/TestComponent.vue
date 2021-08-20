29997b0cd40f1a9b38b3f36d4b557f86bcfd5285
luca <luca.lorenzetti0@gmail.com>
2021-08-20 10:53:36 +0200

    init project

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    {{text}}
+
+</div>
+</template>
+<script>
+export default {
+    
+    name: "TestComponent",
+
+    data(){
+        return{
+            text: "Ciao a tutti"
+        }
+    },
+
+}
+</script>
---
+++ src/views/TestView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="testView">
+    <test-comp/>
+  </div>
+</template>
+<script>
+import TestComp from "@/components/TestComponent"
+
+export default {
+
+components: {
+    TestComp
+  }
+}
+</script>
