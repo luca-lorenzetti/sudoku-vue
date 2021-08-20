b0c72c5b5eea0e493978efb79322345822053f5f
luca <luca.lorenzetti0@gmail.com>
2021-08-20 12:15:22 +0200

    add logo and title

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueMeta from 'vue-meta'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+Vue.use(VueMeta)
 
 Vue.config.productionTip = false;
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="logo p-5 bg-light border rounded">
+      <img alt="Vue logo" src="../assets/logo.png" />
+    </div>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+
 
 export default {
+  data () {
+    return {
+    }
+  },
+  props: {
+  },
   name: "Home",
+  metaInfo() {
+        return { 
+          title: "Sudoku",
+        }
+    },
   components: {
-    HelloWorld,
+  
   },
 };
 </script>
+
+<style lang="scss">
+.logo{
+  text-align: center;
+}
+</style>
