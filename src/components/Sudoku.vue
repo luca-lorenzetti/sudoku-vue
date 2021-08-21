b433c89be844dfd8d950cef20ed042785eb34b6c
luca <luca.lorenzetti0@gmail.com>
2021-08-21 18:35:48 +0200

    fix table

--- src/components/Sudoku.vue
+++ src/components/Sudoku.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="sudoku">
-    <h1>table</h1>
+    <table class="table">
+      <tbody>
+        <tr v-for="(row, indexRow) in 9" v-bind:key="indexRow">
+          <td class="cell" v-for="(cell, indexCell) in matrix[row-1]" v-bind:key="indexCell"> <input type="text" id="inputNumber" name="inputNumber" maxlength="1" v-bind:value="cell" max="9" min="1"></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
   name: "Sudoku",
+  data:function(){
+    return{
+      matrix:[[0,0,0,0,0,0],[0,0,0,0,0]]
+    }
+  },
+  created(){
+    this.$store.commit('fillMatrixWithZero');
+    this.matrix = this.$store.getters.getMatrix;
+  },
+  mounted(){
+    console.log(this.matrix[0])
+  }
 
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+tr{
+  max-width: 500px !important;
+}
+.cell{
+  padding: 0;
+    max-width: 80px !important;
+
+  input{
+    text-align: center;
+    font-size: 30px;
+    width: 80px;
+    height: 80px;
+  }
+}
+
+
 h3 {
   margin: 40px 0 0;
 }
--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,32 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    matrix: []
+  },
+  mutations: {
+    fillMatrixWithZero(state){
+
+      for (let i = 0; i < 9; i++) {
+          let arr = [];
+  
+          for (let j = 0; j < 9; j++) {
+              arr.push(0);
+          } 
+          state.matrix.push(arr);       
+      }
+
+    }
+  },
+  actions: {
+    fillMatrixWithZero(state){
+      state.commit('fillMatrixWithZero')
+    }
+  },
+  getters:{
+    getMatrix(state){
+      return state.matrix;
+    }
+  },
   modules: {},
 });
